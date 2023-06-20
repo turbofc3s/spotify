c845600c6225b7cd797a4e5a6cd0d7aa0da3e628
jeffrey burke <burkejeffrey56@yahoo.com>
2023-06-20 18:02:03 -0400

    making api calls

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
 var client_id = 'e700c58ae14e4e0db1a89f152c923f9e'; // Your client id
 var client_secret = 'e97bc1e59edf485eaff0238479fe4bd0'; // Your secret
 
- const [token, setToken] = useState('')
+ const [token, setToken] = useState('');
+ const [searchTrack, setSearchTrack] = useState('');
 
  useEffect(() => {
   axios('https://accounts.spotify.com/api/token', {
@@ -20,13 +21,32 @@ var client_secret = 'e97bc1e59edf485eaff0238479fe4bd0'; // Your secret
   .then(tokenResponse => {
     console.log(tokenResponse.data.expires_in);
     setToken(tokenResponse.data.access_token);
+  
+  axios ('https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AJanet%2520Jackson&type=track', {
+    method: 'GET',
+    headers: {'Authorization':'Bearer' +  tokenResponse.data.access_token}
+  })
+  .then(searchResponse => {
+    setSearchTrack(searchResponse.data.search.items)
+
+  })  
+
+
 });
 
 }, []);
 
 
-  return <div>{token}</div>        
+  return (
+    <div>
+     <div>{token}</div>
+     <div>{searchTrack}</div>
+    </div> 
+
+
+  )          
 } 
 
 export default App;
+
 
