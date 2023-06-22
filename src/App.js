d52f0053173205428f95d58a8113c4c6c87eeefb
jeffrey burke <burkejeffrey56@yahoo.com>
2023-06-22 13:24:35 -0400

    trouble with api

--- src/App.js
+++ src/App.js
@@ -9,33 +9,45 @@ var client_secret = 'e97bc1e59edf485eaff0238479fe4bd0'; // Your secret
  const [token, setToken] = useState('');
  const [searchTrack, setSearchTrack] = useState('');
 
- useEffect(() => {
+ useEffect (() => {
   axios('https://accounts.spotify.com/api/token', {
     headers: {
-    'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
-    'Content-Type' :' application/x-www-form-urlencoded'
-  },
+      'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+      'Content-Type' :' application/x-www-form-urlencoded'
+    },
     data: 'grant_type=client_credentials',
-    method: 'POST'
-  })
+    method: 'POST'    
+   })
   .then(tokenResponse => {
     console.log(tokenResponse.data.expires_in);
     setToken(tokenResponse.data.access_token);
-  
-  axios ('https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AJanet%2520Jackson&type=track', {
+
+
+  axios('https://api.spotify.com/v1/search?seal&type=album' , {
     method: 'GET',
-    headers: {'Authorization':'Bearer' +  tokenResponse.data.access_token}
-  })
+    headers: {
+      'Authorization':'Bearer' +  tokenResponse.data.access_token},
+    // params: {
+    //   query: 'janet jackson'
+    // }
+    })
   .then(searchResponse => {
     setSearchTrack(searchResponse.data.search.items)
-
-  })  
+  }) ; 
 
 
 });
+  
 
-}, []);
 
+   }, []);
+
+
+
+
+ 
+  
+  
 
   return (
     <div>
