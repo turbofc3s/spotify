3799dac7b70687255a5856aaebc95c3d3be3623f
jeffrey burke <burkejeffrey56@yahoo.com>
2023-08-04 11:58:20 -0400

    getting search to grab a token and then make a request

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // to use component library
 import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
 
-function App() {  
 
-var client_id = 'e700c58ae14e4e0db1a89f152c923f9e'; // Your client id
-var client_secret = 'e97bc1e59edf485eaff0238479fe4bd0'; // Your secret
+function App() { 
+  // console.log(process.env.REACT_APP_client_id)
+  
+var client_id = process.env.REACT_APP_client_id; // Your client id
+var client_secret = process.env.REACT_APP_client_secret; // Your secret
 
  const [token, setToken] = useState('');
  const [searchInput, setSearchInput] = useState('');
@@ -40,11 +42,26 @@ let searchParameters = {
     'Authorization':'Bearer ' +  token}
   }
 
+axios('https://accounts.spotify.com/api/token', {
+    headers: {
+      'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+      'Content-Type' :' application/x-www-form-urlencoded'
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST'    
+   })
+  .then(tokenResponse => {
+    console.log(tokenResponse.data);
+    setToken(tokenResponse.data.access_token);
+    });  
+ 
+
 let artistID = await axios('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
     .then(searchResponse => {
     console.log(searchResponse)
     return searchResponse.data.artists.items[0].id          
    });
+    console.log(token)
    console.log('Artistid is ' + artistID )
 
 
