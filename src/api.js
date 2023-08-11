f6d045973d7a1b54ab60206cb5ee4cdfc15aab15
jeffrey burke <burkejeffrey56@yahoo.com>
2023-08-10 20:39:46 -0400

    errors gone

--- src/api.js
+++ src/api.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Card, Row} from 'react-bootstrap';
 
-function App() { 
-  
+function musicData () {
+
 var client_id = process.env.REACT_APP_client_id; 
 var client_secret = process.env.REACT_APP_client_secret; 
 
  const [token, setToken] = useState('');
  const [searchInput, setSearchInput] = useState('');
  const [tracks, setTracks] = useState([])
- 
+
  useEffect (() => {
   axios('https://accounts.spotify.com/api/token', {
     headers: {
@@ -58,46 +56,6 @@ await axios('https://api.spotify.com/v1/artists/' + artistID + '/top-tracks?mark
    }) ;  
 } 
 
-  return (
-    
-    <div className='App'>      
-      <Container>
-        <h1>Spotify App</h1>
-        <InputGroup className='mb-3' size='lg' >
-           <FormControl
-             placeholder="Search An Artist For Top Tracks"
-             type='input'
-             onKeyPress={event => {
-              if (event.key === 'Enter') {
-                search(); 
-              }
-             }}
-             onChange={event => setSearchInput(event.target.value)}
-           />
-           <Button onClick={search}>
-             Search
-           </Button>
-        </InputGroup>
-      </Container>      
-      <Container>
-        <Row className="mx-2 row row-cols-4">
-          {tracks.map( ( track, i) => {            
-            return (
-              <Card key={i}>
-              <Card.Img src={tracks[i].album.images[0].url} />
-              <Card.Body>
-                <Card.Title><a href= {tracks[i].external_urls.spotify} target='_blank' rel="noreferrer">{tracks[i].name}</a></Card.Title>
-              </Card.Body>
-            </Card>
-            )
-          })}                      
-        </Row>         
-      </Container>
-    </div> 
-  );          
-} 
-
-export default App;
-
+export default musicData;
 
 
